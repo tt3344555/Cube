9ac74cc97c96085e1c8f0ed1f07a6b29fb152430
tt3344555 <142104512+tt3344555@users.noreply.github.com>
2023-08-28 23:11:08 +0400

    first

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-sCube = ''
+sCube = 'GGGGGGGGGBBBBBBBBBRRRRRRRRROOOOOOOOOHHHHHHHHHYYYYYYYYY'
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi(sCube)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
