59a9e4be43042287cae90ec723a7c61eaba625e6
tt3344555 <142104512+tt3344555@users.noreply.github.com>
2023-09-06 02:05:29 +0400

    first

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-SideIndex = {'front': 0, 'back': 1, 'left': 2, 'right': 3, 'down': 4, 'top': 5}
+SideIndex = {'front': 0, 'back': 1, 'left': 2, 'right': 3, 'down': 4, 'up': 5}
 dTurn = ['clock', 'unclock']
 vCube = 'OOOOOOOOO.RRRRRRRRR.GGGGGGGGG.BBBBBBBBB.WWWWWWWWW.YYYYYYYYY.'
 
 
 def get_side(side: str) -> str:
     global vCube
-    if side in ('front', 'back', 'left', 'right', 'down', 'top'):
+    if side in ('front', 'back', 'left', 'right', 'down', 'up'):
         res = vCube[SideIndex[side] * 10:SideIndex[side] * 10 + 9]
         return res
     else:
         print('get_side: error: set side: front, back, left, right, down, top')
-        exit(0)
+        exit(1)
 
 
 def set_side(side: str, value: str):
     global vCube
-    if side in ('front', 'back', 'left', 'right', 'down', 'top'):
+    if side in ('front', 'back', 'left', 'right', 'down', 'up'):
         if SideIndex[side] != 0:
             vCube = vCube[0:SideIndex[side] * 10 - 1] + '.' + value + '.' + vCube[SideIndex[side] * 10 + 10:len(vCube)]
         else:
             vCube = value + '.' + vCube[10:len(vCube)]
     else:
         print('set_side: error: set side: front, back, left, right, down, top')
-        exit(0)
+        exit(1)
+
 
 def turn_side(side: str, turn: dTurn, count: int):
     res = get_side(side)
@@ -37,7 +38,7 @@ def turn_side(side: str, turn: dTurn, count: int):
             res = res[2] + res[5] + res[8] + res[1] + res[4] + res[7] + res[0] + res[3] + res[6]
     else:
         print('turn_side: error: set turn: clock, unclock')
-        exit(0)
+        exit(1)
     set_side(side, res)
 
 
@@ -101,84 +102,88 @@ def turn_side_ext(side1: str, s11, s12, s13: int, side2: str, s21, s22, s23: int
 # front
 def move_front_clock():
     turn_side('front', 'clock', 1)
-    turn_side_ext('top', 6, 7, 8, 'right', 0, 3, 6,
-                  'down', 0, 1, 2, 'left', 2, 5, 8)
+    turn_side_ext('up', 6, 7, 8, 'right', 0, 3, 6,
+                  'down', 2, 1, 0, 'left', 8, 5, 2)
 
 
 def move_front_unclock():
     turn_side('front', 'unclock', 1)
-    turn_side_ext('top', 6, 7, 8, 'left', 8, 5, 2,
+    turn_side_ext('up', 6, 7, 8, 'left', 8, 5, 2,
                   'down', 2, 1, 0, 'right', 0, 3, 6)
 
 
 # back
 def move_back_clock():
     turn_side('back', 'clock', 1)
-    turn_side_ext('top', 0, 1, 2, 'left', 6, 3, 0,
+    turn_side_ext('up', 0, 1, 2, 'left', 6, 3, 0,
                   'down', 8, 7, 6, 'right', 2, 5, 8)
 
 
 def move_back_unclock():
     turn_side('back', 'unclock', 1)
-    turn_side_ext('top', 0, 1, 2, 'right', 2, 5, 8,
+    turn_side_ext('up', 0, 1, 2, 'right', 2, 5, 8,
                   'down', 8, 7, 6, 'left', 6, 3, 0)
 
 
 # left
 def move_left_clock():
     turn_side('left', 'clock', 1)
-    turn_side_ext('top', 0, 3, 6, 'front', 0, 3, 6,
+    turn_side_ext('up', 0, 3, 6, 'front', 0, 3, 6,
                   'down', 0, 3, 6, 'back', 8, 5, 2)
 
 
 def move_left_unclock():
     turn_side('left', 'unclock', 1)
-    turn_side_ext('top', 0, 3, 6, 'back', 8, 5, 2,
+    turn_side_ext('up', 0, 3, 6, 'back', 8, 5, 2,
                   'down', 0, 3, 6, 'front', 0, 3, 6)
 
 
 # right
 def move_right_clock():
     turn_side('right', 'clock', 1)
-    turn_side_ext('top', 2, 5, 8, 'back', 6, 3, 0,
+    turn_side_ext('up', 2, 5, 8, 'back', 6, 3, 0,
                   'down', 2, 5, 8, 'front', 2, 5, 8)
 
 
 def move_right_unclock():
     turn_side('right', 'unclock', 1)
-    turn_side_ext('top', 2, 5, 8, 'front', 2, 5, 8,
+    turn_side_ext('up', 2, 5, 8, 'front', 2, 5, 8,
                   'down', 2, 5, 8, 'back', 6, 3, 0)
 
 
 # top
-def move_top_clock():
-    turn_side('top', 'clock', 1)
+def move_up_clock():
+    turn_side('up', 'clock', 1)
     turn_side_ext('front', 0, 1, 2, 'left', 0, 1, 2,
                   'back', 0, 1, 2, 'right', 0, 1, 2)
 
 
-def move_top_unclock():
-    turn_side('top', 'unclock', 1)
+def move_up_unclock():
+    turn_side('up', 'unclock', 1)
     turn_side_ext('front', 0, 1, 2, 'right', 0, 1, 2,
-                  'back', 0, 1, 2, 'left', 2, 1, 0)
+                  'back', 0, 1, 2, 'left', 0, 1, 2)
 
 
 # down
 def move_down_clock():
-    turn_side('do', 'clock', 1)
-    turn_side_ext('front', 6, 7, 8,                  'right', 6, 7, 8,
-                  'back', 6, 7, 8,                  'left', 6, 7, 8)
+    turn_side('down', 'clock', 1)
+    turn_side_ext('front', 6, 7, 8, 'right', 6, 7, 8,
+                  'back', 6, 7, 8, 'left', 6, 7, 8)
 
 
 def move_down_unclock():
     turn_side('down', 'unclock', 1)
-    turn_side_ext('front', 6, 7, 8,                  'left', 6, 7, 8,
-                  'back', 6, 7, 8,                  'right', 6, 7, 8)
+    turn_side_ext('front', 6, 7, 8, 'left', 6, 7, 8,
+                  'back', 6, 7, 8, 'right', 6, 7, 8)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print(vCube)
-#    move_down_unclock()
-    move_down_clock()
+    for i in range(6):
+       move_back_clock()
+       move_left_clock()
+       move_back_unclock()
+       move_left_unclock()
+
     print(vCube)
