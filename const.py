7750bce3051354eba5907cd9a8c7508291b1e871
tt3344555 <142104512+tt3344555@users.noreply.github.com>
2023-09-20 01:04:56 +0400

    more move

--- const.py
+++ const.py
@@ -22,6 +22,23 @@ F2L = {'F2L_ALGORITM_01': "R U' R' U y' R' U2 R U' U' R' U R",
         'F2L_ALGORITM_19': "R U' R' U2 R U R'",
         'F2L_ALGORITM_20': "U R U' U' R' U R U' R'",
         'F2L_ALGORITM_21': "R U' U' R' U' R U R'"}
-
-
-
+# 22 U' R U' R' U2 R U' R'
+# 23 U' R U R' y' U R' U' R
+# 24 y' U R' U R U' R' U' R
+# 25 y' R' U' R
+# 26 y' U R' U' R U' R' U' R
+# 27 y' R U' U' R2' U' R2 U' R'
+# 28 l U r U' r' U' l'
+# 29 U' R U' U' R' U2 R U' R'
+# 30 U' R U R' U' R U' U' R'
+# 31 U R U' R'
+# 32 U' R U' U' R' U R U R'
+# 33 y' U R' U' R d R U R'
+# 34 y' U' R' U R
+# 35 y' U R' U' R U' U' R' U R
+# 36 y' U R' U2 R U' U' R' U R
+# 37 R U' R' U2 y' R' U' R
+# 38 R U' R' U R U' R' U2 R U' R'
+# 39 U' R U R' U R U R'
+# 40 R U R'
+# 41 U' R U' R' U R U R'
